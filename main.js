a9cd7d9aba3e7275106313241df2f7e1620c99a6
Chef NoName <abdijama21@gmail.com>
2021-02-14 16:14:55 +0000

    1st Commit

---
+++ main.js
@@ -0,0 +1,23 @@
+const modal = document.querySelector(".modal");
+const previews = document.querySelectorAll(".gallery img");
+const original = document.querySelector(".full-img");
+const caption = document.querySelector(".caption");
+
+previews.forEach((preview) => {
+  preview.addEventListener("click", () => {
+    modal.classList.add("open");
+    original.classList.add("open");
+    //Dynamic change text and image
+    const originalSrc = preview.getAttribute("data-original");
+    original.src = `./assets/${originalSrc}`;
+    const originalText = preview.getAttribute("alt");
+    caption.textContent = originalText;
+  });
+});
+
+modal.addEventListener("click", (e) => {
+  if (e.target.classList.contains("modal")) {
+    modal.classList.remove("open");
+    original.classList.remove("open");
+  }
+});
